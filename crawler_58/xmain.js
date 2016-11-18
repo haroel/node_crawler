091e0939406cf7570bef9c671c2de73fd3496816
ihowe <ihowe@outlook.com>
2016-11-18 19:19:19 +0800

    爬 58 网站 图片

--- crawler_58/xmain.js
+++ crawler_58/xmain.js
@@ -4,10 +4,14 @@
 'use strict';
 
 
-let cheerio = require("cheerio");
 let fs = require("fs");
 let http = require('http');
 let url = require("url");
+let path = require("path");
+
+
+let request = require("request");
+let cheerio = require("cheerio");
 
 
 let $lib = require("../libs/$lib.js");
@@ -28,26 +32,42 @@ function startCrawler ( _url_ )
             let mainUrlObj = url.parse(_url_);
             let $ = cheerio.load(html);
             let infolist = $('.main').find('#infolist').find('table').find('tr');
+            trace(`爬取到${infolist.length}条记录`);
             let _log = "";
-            infolist.each(function()
+            let p = Promise.resolve();
+            infolist.each(function(id,element)
             {
-                let _tr = $(this);
+                let _tr = $(element);
                 let title = _tr.find("td:nth-child(2)").find("a:first-child").text();
                 let price = _tr.find("td:last-child").find("b:first-child").text();
+                let img = _tr.find("td:first-child").find("img").attr("lazy_src");
 
                 if (title !== "")
                 {
                     let log = `${title} ${' '.repeat( Math.max(1, NUM - title.length))}  价格:${price}\n`;
                     _log += log;
                 }
+                if (img)
+                {
+                    let fileName = path.basename(img);
+                    p = p.then(()=>
+                    {
+                        trace("下载成功",img);
+                        return $lib.getImage(img,path.join(__dirname,"img",fileName) );
+                    });
+                }
             });
-            trace(`爬取到${infolist.length}条记录`);
-            fs.appendFile(__dirname + "_log.txt",_log,"utf-8", function () {
+            p.catch(function(e) {
+                trace("下载错误,",e,e.url,e.savedPath);
+            }).then(function() {
+                let nextPageUrl = $('.main').find(".pager").find("a.next").attr("href");
+                mainUrlObj.pathname = nextPageUrl;
+                mainUrlObj.path = nextPageUrl;
+                startCrawler(url.format(mainUrlObj));
             });
-            let nextPageUrl = $('.main').find(".pager").find("a.next").attr("href");
-            mainUrlObj.pathname = nextPageUrl;
-            mainUrlObj.path = nextPageUrl;
-            startCrawler(url.format(mainUrlObj));
+            fs.appendFile( path.join(__dirname,"_log.txt"),_log,"utf-8", function () {
+            });
+
         })
         .catch(function (err) {
             trace(err);
--- libs/$lib.js
+++ libs/$lib.js
@@ -3,6 +3,10 @@
  */
 let http = require('http');
 let url = require("url");
+let fs = require("fs");
+
+
+let request = require("request");
 
 let trace = console.log;
 
@@ -26,4 +30,54 @@ module.exports.getWebPageContent = function (_url) {
         });
     };
     return new Promise(handler);
+};
+
+module.exports.getHtml = function (url) {
+    return new Promise(
+        (resolve,reject)=>
+        {
+            request(url, (error,response,body) =>
+            {
+                if (error)
+                {
+                    reject(error);
+                }else
+                {
+                    if (response.statusCode == 200)
+                    {
+                        resolve(body)
+                    }
+                }
+            })
+        }
+    )
+};
+
+module.exports.getImage = function (url, savedPath ) {
+    return new Promise(
+        (resolve,reject)=>
+        {
+            request({uri: url, encoding: 'binary'}, function (error, response, body)
+            {
+                if (!error ) {
+                    if (response.statusCode == 200)
+                    {
+                        fs.writeFile(savedPath, body, 'binary', function (err) {
+                            if (err) {
+                                error.url = url;
+                                error.savedPath = savedPath;
+                                reject(error);
+                            } else {
+                                resolve(url, savedPath);
+                            }
+                        });
+                    }
+                }else {
+                    error.url = url;
+                    error.savedPath = savedPath;
+                    reject(error);
+                }
+            });
+        }
+    )
 };
